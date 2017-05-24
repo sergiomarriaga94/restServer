9da9ee97657b1317d1e8ec2ee9199601bcebea2d
Sergio Marriaga <sergio.marriaga@PERCEPTIO.LOCAL>
2017-05-24 16:27:51 -0500

    configuracion travis

--- target/generated-sources/annotations/com/udea/servicio/modelo/Customer_.java
+++ target/generated-sources/annotations/com/udea/servicio/modelo/Customer_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-24T16:10:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-24T16:19:30")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
--- target/generated-sources/annotations/com/udea/servicio/modelo/DiscountCode_.java
+++ target/generated-sources/annotations/com/udea/servicio/modelo/DiscountCode_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-24T16:10:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-24T16:19:30")
 @StaticMetamodel(DiscountCode.class)
 public class DiscountCode_ { 
 
